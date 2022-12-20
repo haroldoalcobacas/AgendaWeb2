b1b4db26706624fed30e16d6a7c5d80873ee0104
haroldo <haroldo.alcobacas@gmail.com>
2022-12-19 21:11:13 -0300

    Lista de Contatos

--- src/main/java/com/plix/agendaweb2/controllers/ContatoController.java
+++ src/main/java/com/plix/agendaweb2/controllers/ContatoController.java
@@ -1,15 +1,35 @@
 package com.plix.agendaweb2.controllers;
 
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+import com.plix.agendaweb2.models.Contato;
+import com.plix.agendaweb2.repositories.ContatoRepository;
 
 @Controller
 public class ContatoController {
 	
-	@GetMapping("/contatos")
-	public String index() {
-		return "contatos/index";
+	
+	@Autowired
+	private ContatoRepository contatoRepository;
 		
+	
+	@GetMapping("/contatos")
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("contatos/index");
+		List<Contato> contatos = contatoRepository.findAll();
+		 mv.addObject("contatos", contatos);
+		 return mv;
+
+			
+		 
 	}
 	
 
--- src/main/java/com/plix/agendaweb2/repositories/ContatoRepository.java
+++ src/main/java/com/plix/agendaweb2/repositories/ContatoRepository.java
@@ -1,10 +1,12 @@
 package com.plix.agendaweb2.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.plix.agendaweb2.models.Contato;
 
 
-
+@Repository
 public interface ContatoRepository extends JpaRepository<Contato, Long>{
 
 	
